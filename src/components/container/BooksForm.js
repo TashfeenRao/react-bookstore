ecf6f3e2a13410b6f2507f229a0a50ffdace4791
tashfeenrao <raotashfeen25@gmail.com>
2020-07-09 01:45:08 +0500

    bookform component own state updated on change values

--- src/components/container/BooksForm.js
+++ src/components/container/BooksForm.js
@@ -1,21 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function BooksForm() {
-  const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
-  return (
-    <div className="booksForm">
-      <h2>BooksForm</h2>
+export default class BooksForm extends Component {
+  constructor(props) {
+    super(props);
 
-      <form>
-        <input type="text" placeholder="Title" name="title" required />
-        <select name="category">
-          {categories.map(cat => (
-            <option key={cat}>{cat}</option>
-          ))}
-        </select>
-        <button type="button">Add Book</button>
+    this.state = {
+      id: Math.floor(Math.random() * 1000),
+      title: null,
+      category: null,
+    }
 
-      </form>
-    </div>
-  );
+    this.categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+    this.handleChange = this.handleChange.bind(this);
+  }
+  
+  handleChange = (e) => {
+    e.preventDefault();
+    let formvalues = this.state;
+    let name = e.target.name;
+    let value = e.target.value;
+    formvalues[name] = value;
+    this.setState({formvalues});
+
+  }
+
+  render () {
+
+    return (
+      <div className="booksForm">
+        <h2>BooksForm</h2>
+  
+        <form>
+          <input type="text" placeholder="Title" name="title" onChange={this.handleChange} required />
+          <select name="category" onChange={this.handleChange}>
+            {this.categories.map(cat => (
+              <option key={cat}>{cat}</option>
+            ))}
+          </select>
+          <button type="button">Add Book</button>
+  
+        </form>
+      </div>
+    );
+  }
 }
